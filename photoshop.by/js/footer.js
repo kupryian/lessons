70734a5a17b8f8e11e9da8de300e5948f915eaa4
kupryian <60928526+kupryian@users.noreply.github.com>
2022-06-09 20:02:29 +0300

    lesson202

---
+++ photoshop.by/js/footer.js
@@ -0,0 +1,61 @@
+var footer = document.querySelector('footer');
+var footerIn =`<div class="footer_top">
+<div class="contain">
+    <ul class="footer_ul">
+        <li>
+            <ul>
+                <li>Фотоаппараты</li>
+                <li>Объективы</li>
+                <li>Студийный свет</li>
+                <li>Аксессуары</li>
+                <li>Вспышки</li>
+                <li>Студийный свет</li>
+                <li>Печать</li>
+                <li>Гаджеты</li>
+            </ul>
+        </li>
+        <li>
+            <ul>
+                <li>Фотомагазин</li>
+                <li>Фотопракат</li>
+                <li>Трейд-Ин</li>
+                <li>Доставка</li>
+                <li>Как купить</li>
+                <li>Способы оплаты</li>
+                <li>Гарантия и возврат</li>
+                <li>Сертификаты</li>
+                <li>Скидки</li>
+            </ul>
+        </li>
+        <li>
+            <ul>
+                <li>О компании</li>
+                <li>Новости</li>
+                <li>Акции</li>
+                <li>Обзоры</li>
+                <li>Контакты</li>
+                <li>Вакансии</li>
+                <li>Партнеры</li>
+                <li>Отзывы</li>
+                <li>Карта сайта</li>
+            </ul>
+        </li>
+        <li>
+            <ul>
+                <li><b>Коктактная информация</b></li>
+                <li>г.Минск ул. Победителей д 3 пом 41</li>
+                <li>fotomagaz@gmail.com</li>
+                <li><br></li>
+                <li><br></li>
+                <li>способы оплаты</li>
+                <li><img src="img/image 48.png" alt=""><img src="img/image 49.png" alt=""><img src="img/image 50.png" alt=""></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+</div>`;
+    footer.innerHTML = footerIn;
+    console.log("footer ok")
+
