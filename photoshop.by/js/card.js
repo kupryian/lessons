0afdd65fcc62a52954678951e74d1f8fdbca9ee6
kupryian <60928526+kupryian@users.noreply.github.com>
2022-06-09 20:01:19 +0300

    lesson201

---
+++ photoshop.by/js/card.js
@@ -0,0 +1,24 @@
+function pre_photo_1() {
+    document.querySelector(".big_photo img:first-child").style.display="inline";
+    document.querySelector(".big_photo img:nth-child(2)").style.display="none";
+    document.querySelector(".big_photo img:nth-child(3)").style.display="none";
+    document.querySelector(".big_photo img:nth-child(4)").style.display="none";
+}
+function pre_photo_2() {
+    document.querySelector(".big_photo img:first-child").style.display="none";
+    document.querySelector(".big_photo img:nth-child(2)").style.display="inline";
+    document.querySelector(".big_photo img:nth-child(3)").style.display="none";
+    document.querySelector(".big_photo img:nth-child(4)").style.display="none";
+}
+function pre_photo_3() {
+    document.querySelector(".big_photo img:first-child").style.display="none";
+    document.querySelector(".big_photo img:nth-child(2)").style.display="none";
+    document.querySelector(".big_photo img:nth-child(3)").style.display="inline";
+    document.querySelector(".big_photo img:nth-child(4)").style.display="none";
+}
+function pre_photo_4() {
+    document.querySelector(".big_photo img:first-child").style.display="none";
+    document.querySelector(".big_photo img:nth-child(2)").style.display="none";
+    document.querySelector(".big_photo img:nth-child(3)").style.display="none";
+    document.querySelector(".big_photo img:nth-child(4)").style.display="inline";
+}
