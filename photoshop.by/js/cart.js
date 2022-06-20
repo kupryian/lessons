10df654d0fd4d6c5f537898b3f55af38430f9531
kupryian <60928526+kupryian@users.noreply.github.com>
2022-06-20 18:12:54 +0300

    lesson20

--- photoshop.by/js/cart.js
+++ photoshop.by/js/cart.js
@@ -1,15 +1,15 @@
 function up1() {
 var number = document.querySelector('.product_number');
-var getNumber = document.querySelector('.product_number').innerHTML;
-var chislo = Number (getNumber);
-var numberProduct =chislo+1;
+var getNumber = Number (document.querySelector('.product_number').innerHTML);
+var numberProduct =getNumber+1;
 number.innerHTML = numberProduct;
+
+
 }
 function down1() {
     var number = document.querySelector('.product_number');
-    var getNumber = document.querySelector('.product_number').innerHTML;
-    var chislo = Number (getNumber);
-    var numberProduct =chislo-1;
+    var getNumber = Number (document.querySelector('.product_number').innerHTML);
+    var numberProduct =getNumber-1;
     numberProduct<1 ? numberProduct = 1 : 
     number.innerHTML = numberProduct;
     }
